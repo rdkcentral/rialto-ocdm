7689a61bdf1c4fc7bc7cb15ca9d27a0d37345714
Marcin Wojciechowski <marcin.wojciechowski@sky.uk>
2026-02-19 13:31:14 +0100

    comment change

--- library/source/OpenCDMSessionPrivate.cpp
+++ library/source/OpenCDMSessionPrivate.cpp
@@ -207,7 +207,7 @@ bool OpenCDMSessionPrivate::getChallengeDataSize(uint32_t &size, bool isLdl)
     // Challenge will be reinitialized. Clear any previous data.
     {
         std::unique_lock<std::mutex> lock{m_mutex};
-        // Temporary hack - wait for previous challenge to be received before clearing it.
+        // Wait for previous challenge to be received before clearing it.
         m_challengeCv.wait_for(lock, std::chrono::milliseconds{250}, [this]() { return !m_challengeData.empty(); });
         m_challengeData.clear();
     }

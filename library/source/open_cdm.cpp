fd5c85adaac2ed173967c3b47efa14e282e1d6f6
varatharajan568 <130632918+varatharajan568@users.noreply.github.com>
2026-08-24 14:15:26 +0530

    Update open_cdm.cpp

--- library/source/open_cdm.cpp
+++ library/source/open_cdm.cpp
@@ -245,6 +245,7 @@ OpenCDMError opencdm_construct_session(struct OpenCDMSystem *system, const Licen
         ActiveSessions::instance().remove(newSession);
         return ERROR_FAIL;
     }
+
     std::vector<uint8_t> cdmDataVec;
     if (CDMData && CDMDataLength > 0)
     {
